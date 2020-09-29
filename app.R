f671372bfea1056949b1e3eeb4880fc4930bb5a2
Milind Parvatia <milind1997@gmail.com>
2020-09-29 23:50:21 +1000

    v2.0
    Plot 1,2,3 and 5 are updated

--- app.R
+++ app.R
@@ -22,11 +22,17 @@ library(lubridate)
 #importing dataset
 covid_data <- read_csv("covid-data.csv")
 
+#importing shrunk gdp data
+Q2_gdp <- read_csv("economic-decline-in-the-second-quarter-of-2020.csv")
+
 #targeting the latest date
 latestDate <- max(covid_data$date)
 
 #freezing the data
 freezed_data <- covid_data %>% filter(date==latestDate)
+
+#Merging the datasets
+freezed_data$shrunk_gdp <- (Q2_gdp$`GDP growth from previous year, 2020 Q2`[match(freezed_data$location, Q2_gdp$Entity)])
 
 #Filtering the data continent wise
 Africa <-freezed_data %>% filter(continent == 'Africa')
@@ -35,9 +41,6 @@ Europe <-freezed_data %>% filter(continent == 'Europe')
 North_America <-freezed_data %>% filter(continent == 'North America')
 Oceania <-freezed_data %>% filter(continent == 'Oceania')
 South_America <-freezed_data %>% filter(continent == 'South America')
-
-
-
 
 comb <- freezed_data %>% filter(location == 'Australia' |
                                   location == 'United States' |
@@ -55,15 +58,21 @@ ui <- shinyUI(
     navbarPage("Covid-19 Analysis",
                tabPanel("Plot 1",
                         titlePanel('GDP vs. Hospital beds per Deaths'),
-                        plotlyOutput("casesPlot")
+                        plotlyOutput("plot1")
                         ),
                tabPanel("plot 2",
-                        titlePanel("plot 1"),
-                        plotlyOutput("casesPlot2")
+                        titlePanel("GDP per captcha vs. Human Index"),
+                        plotlyOutput("plot2")
                         ),
-               tabPanel("plot 3"),
+               tabPanel("plot 3",
+                        titlePanel("Effect of stringent measures on covid cases"),
+                        plotlyOutput("plot3")
+                        ),
                tabPanel("plot 4"),
-               tabPanel("plot 5"),
+               tabPanel("plot 5",
+                        titlePanel("Total Death vs. shrunk GDP"),
+                        plotlyOutput("plot5")
+                        ),
                tabPanel("plot 6"),
                tabPanel("Summary")
     ),
@@ -71,13 +80,38 @@ ui <- shinyUI(
 )
 
 server <- function(input, output) {
-  
-  output$casesPlot <- renderPlotly({
-      comb %>%
+  output$plot1 <- renderPlotly({
+    comb %>%
       ggplot(aes(x=gdp_per_capita, y=hospital_beds_per_thousand, size=total_deaths, color=location)) +
       geom_point(alpha=0.5) +
       scale_size(range = c(.1, 24), name="Deaths")
   })
+  
+  output$plot2 <- renderPlotly({
+    freezed_data %>%
+      ggplot(aes(x=human_development_index, y=gdp_per_capita, size=total_deaths_per_million, color=location)) +
+      geom_point(alpha=0.5) +
+      scale_size(range = c(.1, 24), name="Deaths")
+  })
+  
+  output$plot3 <- renderPlotly({
+    ggplot(c,aes(x=stringency_index,y=total_cases,colour=location,group=location,fill=location))+
+      geom_line(size = 0.7)+
+      scale_y_continuous(trans = 'log10') +
+      labs(
+           x = "stringency index",
+           y = "The number of total cases") +
+      theme(axis.text.x = element_text(angle = 90,vjust = 0.5))
+  })
+  
+  output$plot5 <- renderPlotly({
+    freezed_data %>%
+      ggplot(aes(x=shrunk_gdp, y=total_deaths_per_million, size=total_deaths_per_million, color=location)) +
+      geom_point(alpha=0.5) +
+      scale_size(range = c(.1, 24), name="Deaths")
+  })
+  
+
 }
 # Run the application 
 shinyApp(ui = ui, server = server)
